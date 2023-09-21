b138a76c36554983061b1be5e71bff745d6d9dfa
Akilaydin <akilaydin2@gmail.com>
2023-09-21 13:23:49 +0300

    Initial commit

---
+++ OriGames.Facts.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OriGames.Facts.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
